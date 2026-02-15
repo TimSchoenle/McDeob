c3a65a8626575e8eaea1b07335fc83139986f25f
Tim Schönle <13711365+TimSchoenle@users.noreply.github.com>
2026-02-15 06:49:31 +0100

    build: improve ci (#121)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,9 @@ javafx {
 }
 
 group = "com.shanebeestudios"
-project.version = file("version.txt").readText().trim().replace("\n", "")
+// x-release-please-start-version
+version = "2.8.0"
+// x-release-please-end
 description = "McDeob"
 
 application {
